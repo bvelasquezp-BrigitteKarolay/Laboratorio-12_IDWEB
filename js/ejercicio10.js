15f1dc1efb068320482ddffd31d2e355357c4cdf
Brigitte Karolay <bvelasquezp@unsa.edu.pe>
2025-10-29 15:49:58 -0500

    Delete js directory

--- js/ejercicio10.js
+++
@@ -1,9 +0,0 @@
-// FUNCION FLECHA saludo QUE RECIBE UN NOMBRE Y DEVUELVE UN MENSAJE
-const saludo = (nombre) => "Hola " + nombre + ", bienvenido!";
-
-// FUNCION PARA EJECUTAR
-function ejecutarEjercicio10() {
-  const nombre = prompt("Ingrese su nombre:");
-  const mensaje = saludo(nombre);
-  document.getElementById("resultado10").innerText = mensaje;
-}
